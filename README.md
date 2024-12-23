a24cc7b1b63d5d1ac605878ea7a0392118c014d4
w0dnj5 <jw991211@icloud.com>
2024-12-24 04:25:33 +0900

    fix: AOP @Bean 등록 순환 참조 오류 수정

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
+
+### AOP
+
+- [AOP(TimeTraceAop)를 @Component 로 선언 vs SpringConfig에 @Bean으로 등록](https://www.inflearn.com/community/questions/48156/aop-timetraceaop-%EB%A5%BC-component-%EB%A1%9C-%EC%84%A0%EC%96%B8-vs-springconfig%EC%97%90-bean%EC%9C%BC%EB%A1%9C-%EB%93%B1%EB%A1%9D)
--- src/main/java/hello/hello_spring/aop/TimeTraceAop.java
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -3,8 +3,10 @@ package hello.hello_spring.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TimeTraceAop {
 
     @Around("execution(* hello.hello_spring..*(..))")
