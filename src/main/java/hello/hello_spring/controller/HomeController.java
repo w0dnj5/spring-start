24b2d8e0915fa4c506756365e670466926ec12a6
w0dnj5 <jw991211@icloud.com>
2024-12-24 01:36:46 +0900

    feat: 회원 웹 기능 - 등록

--- src/main/java/hello/hello_spring/controller/HomeController.java
+++ src/main/java/hello/hello_spring/controller/HomeController.java
@@ -1,13 +1,40 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.MemberForm;
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    private MemberService memberService;
+
+    @Autowired
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        
+        return "redirect:/";
     }
 }
